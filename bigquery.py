6bcdc31d1b7290b734ba613c45b8cde3561b5ae4
Reno Greenleaf <reno.greenleaf@gmail.com>
2017-07-03 01:20:55 +0300

    Helps to do requests to Google BigQuery.

---
+++ bigquery.py
@@ -0,0 +1,38 @@
+from google.cloud import bigquery
+
+current = {
+  'client': False,
+  'dataset': False,
+  'table': False
+}
+
+def connect(service_account):
+  current['client'] = bigquery.Client.from_service_account_json(service_account)
+
+def select_dataset(name):
+  for dataset in current['client'].list_datasets():
+
+    if dataset.name == name:
+      current['dataset'] = dataset
+      return True
+
+  return False
+
+def select_table(name):
+  for table in current['dataset'].list_tables():
+
+    if table.name == name:
+      current['table'] = table
+      return True
+
+  return False
+
+def recreate_table(data):
+  current['table'].reload()
+  schema = current['table'].schema[:]
+  name = current['table'].name
+  current['table'].delete()
+  new_table = current['dataset'].table(name, schema)
+  new_table.create()
+  new_table.upload_from_file(data, 'CSV', skip_leading_rows=1)
+  current['table'] = new_table
